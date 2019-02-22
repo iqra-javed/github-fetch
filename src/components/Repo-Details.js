bc614978acc8bdef09e374e14424a65e38410d0d
Iqra Javed <35740252+iqra-javed@users.noreply.github.com>
2019-02-22 05:58:33 -0600

    Add readme url and back btn to Repo-Details component

--- src/components/Repo-Details.js
+++ src/components/Repo-Details.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
-import '../styles/RepoDetails.css'
+import '../styles/RepoDetails.css';
 
-const RepoDetails = props => {
-   
-    let matchedRepo = props.repos.filter(repo => repo.name === props.match.params.name)[0]
- 
-    return (
-        <div>
-            <h3>{matchedRepo.name}</h3>
-            <ul className="repo-info">
-                <li>{`Owner Username: ${matchedRepo.owner.login}`}</li>
-                <li>{`Repo URL: ${matchedRepo.url}`}</li>
-                <li>{`Watchers: ${matchedRepo.watchers}`}</li>
-                
-            </ul>
-        </div>
+class RepoDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      readme: ''
+    };
+  }
+
+  matchedRepo = this.props.repos.filter(
+    repo => repo.name === this.props.match.params.name
+  )[0];
+
+  componentDidMount() {
+    fetch(
+      `https://api.github.com/repos/${this.matchedRepo.owner.login}/${
+        this.matchedRepo.name
+      }/readme`
     )
-}
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject();
+      })
+      .then(data => {
+        console.log(data);
+        this.setState({ readme: data.html_url });
+      })
+      .catch(error => {
+        console.log('Fetching Failed: ', error);
+      });
+  }
 
+  render() {
+    return (
+      <div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <Link to='/repos'>
+            <button
+              style={{ margin: '20px 0', backgroundColor: 'mediumpurple' }}
+            >
+              Back
+            </button>
+          </Link>
+          <h3>{this.matchedRepo.name}</h3>
+        </div>
+
+        <ul className='repo-info'>
+          <li>
+            <strong>Owner Username:</strong>
+            {this.matchedRepo.owner.login}
+          </li>
+          <li>
+            <strong>Repo URL:</strong>
+            <a href={this.matchedRepo.html_url}>{this.matchedRepo.html_url}</a>
+          </li>
+          <li>
+            <strong>Readme:</strong>
+            <a href={this.state.readme}>{this.state.readme}</a>
+          </li>
+          <li>
+            <strong>Watchers:</strong>
+            {this.matchedRepo.watchers}
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default withRouter(RepoDetails);
