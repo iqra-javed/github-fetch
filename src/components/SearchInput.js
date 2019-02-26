dbed805925ff44a4007a6dea1ebf29d14febea9b
Iqra Javed <35740252+iqra-javed@users.noreply.github.com>
2019-02-26 04:54:26 -0600

    Prettier beam in SearchInput.js

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,35 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import '../styles/SearchInput.css';
 
 const SearchInput = props => {
-
-
- 
-    return (
-      <div className='input-container'>
-        {props.error && (
-          <div className='error-message'>
-            <span>
-              {props.error.status === 404 ? '' : props.error.status}
-            </span>
-            {props.error.statusText === 'Not Found'
-              ? 'User not found. Please try again.'
-              : props.error.statusText || props.error}
-          </div>
-        )}
-        <form onSubmit={props.fetchUserHandler}>
+  return (
+    <div className='input-container'>
+      {props.error && (
+        <div className='error-message'>
+          <span>{props.error.status === 404 ? '' : props.error.status}</span>
+          {props.error.statusText === 'Not Found'
+            ? 'User not found. Please try again.'
+            : props.error.statusText || props.error}
+        </div>
+      )}
+      <form onSubmit={props.fetchUserHandler}>
         <input
           type='text'
           placeholder='Enter Github Username'
           name='currentUser'
         />
-          <button className='search-btn'>
-            Search
-          </button>
-          </form>
-      </div>
-    );
-}
+        <button className='search-btn'>Search</button>
+      </form>
+    </div>
+  );
+};
 
 export default SearchInput;
